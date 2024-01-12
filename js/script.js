bc381384590d70ca33e65056adcde224d4ea62f1
stiti <moncef.stiti@icloud.com>
2024-01-13 00:52:08 +0100

    Dernières modifications de la soirée

--- js/script.js
+++ js/script.js
@@ -6,3 +6,20 @@ document.addEventListener('DOMContentLoaded', function () {
         navLinks.classList.toggle('show');
     });
 });
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+// Afficher ou masquer le bouton en fonction de la position de défilement
+document.addEventListener('scroll', function () {
+    const scrollTopBtn = document.getElementById('scrollTopBtn');
+    if (window.scrollY > 200) {
+        scrollTopBtn.style.display = 'block';
+    } else {
+        scrollTopBtn.style.display = 'none';
+    }
+});
