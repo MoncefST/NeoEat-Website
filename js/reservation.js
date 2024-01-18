49173697790b27c073b499036dd2e7f18aeee0ee
Moncef STITI <moncef.stiti@icloud.com>
2024-01-18 13:17:26 +0100

    Modification des chemins etc...

--- js/reservation.js
+++ js/reservation.js
@@ -1,5 +1,5 @@
 // Pour que la date de réservation ne soit jamais inférieur à celle d'aujourd'hui
-document.addEventListener('DOMContentLoaded', function () {
-    const today = new Date().toISOString().split('T')[0];
-    document.getElementById('date').min = today;
+document.addEventListener('DOMContentLoaded', function() {
+    var today = new Date().toISOString().split('T')[0];
+    document.getElementById('date').setAttribute('min', today);
 });
